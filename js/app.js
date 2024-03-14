b2da3805c493f23e5423ee3efd11d5fd72b4dbb1
Cigno05 <paolocalcagni.82@gmail.com>
2024-03-14 10:39:32 +0100

    ADD: function for click and level

--- js/app.js
+++ js/app.js
@@ -4,9 +4,10 @@ console.log('JS funziona');
 const playButton = document.getElementById('play-btn');
 
 // faccio click sul bottone play
-playButton.addEventListener('click', function() {
+playButton.addEventListener('click', playTheGame)
 
-
+//creare la funzione per l'event listner
+function playTheGame () {
     //richiamo l'elemento in cui andrá il div .cell
 let gridElement = document.querySelector('.grid-cells');
 // console.log(gridElement);
@@ -15,8 +16,23 @@ console.log(difficultyChoice.value)
 
 
 //stabilisco quante celle sono presenti in una riga
-let size = parseInt(difficultyChoice.value);
+// let size = parseInt(difficultyChoice.value);
+let size = levelGame()
 
+function levelGame() {
+    let level = 10
+
+    if (parseInt(difficultyChoice.value) === 10) {
+        level = 10
+    } else if (parseInt(difficultyChoice.value) === 9) {
+        level = 9
+    } else {
+        level = 7
+    }
+
+    return level
+
+}
 // stabilisco il numero di celle presenti nella griglia
 const numberOfCells = size * size;
 
@@ -28,10 +44,7 @@ for (let index = 0; index < numberOfCells; index++) {
     const number = index + 1;
     // console.log(number);
     
-    
-
-
-    // creo l'elemento che andró a mettere dentro gridElement
+     // creo l'elemento che andró a mettere dentro gridElement
     const cellElement = document.createElement('div');
     // aggiungo la classe al div
     cellElement.className = 'cell';
@@ -43,8 +56,6 @@ for (let index = 0; index < numberOfCells; index++) {
     //aggiungo l'elemento creato al gridElement
     gridElement.append(cellElement);
     
-    
-    
     //assegno una classe cambia colore all'elemento al momento del click
     cellElement.addEventListener('click', function() {
         // console.log('click on ', number)
@@ -55,7 +66,7 @@ for (let index = 0; index < numberOfCells; index++) {
     }   
     )
 }
-})
+}
 
 
 
